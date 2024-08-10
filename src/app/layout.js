bd68287463b5550f21113ae27dd448080311fe7b
Brian <briantechdom@gmail.com>
2024-08-10 15:03:32 -0700

    Update children tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,11 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
         <div>
           <Navbar />
         </div>
-
+        {children}
       </body>
     </html>
   );
