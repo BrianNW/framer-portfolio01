cf2c34657aa773390d623c0a9759c324a9660c87
Brian <briantechdom@gmail.com>
2024-08-11 19:29:26 -0700

    Add x axis padding for each screensize

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <div className = 'h-full flex items-center justify-between'>
+        <div className = 'h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
             <div className='1'></div>
             <div className='2'></div>
         
