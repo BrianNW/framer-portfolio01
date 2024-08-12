9f598722d50bc10bb272ca79d10fad23e5a46111
Brian <briantechdom@gmail.com>
2024-08-12 14:40:36 -0700

    Add styles to the second span

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,9 +3,9 @@ const Navbar = () => {
         <div className = 'h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
            {/* Logo */}
             <div className='1'>
-                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semi">
+                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold">
                     <span className="text-white">Briannw</span>
-                    <span>.com</span>
+                    <span className ="w-12 h-8 rounded bg-white text-black">.com</span>
                 </Link>
             </div>
 
