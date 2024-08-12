9d87a29fdfc2410dcfbee3ebe61709a68d51ce7d
Brian <briantechdom@gmail.com>
2024-08-12 14:37:12 -0700

    Style first span

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,8 +3,9 @@ const Navbar = () => {
         <div className = 'h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
            {/* Logo */}
             <div className='1'>
-                <Link href="/">
-                    
+                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semi">
+                    <span className="text-white">Briannw</span>
+                    <span>.com</span>
                 </Link>
             </div>
 
