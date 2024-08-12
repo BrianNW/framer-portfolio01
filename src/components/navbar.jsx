cdb303775ec96d814b46c40c442498b8e7d9857d
Brian <briantechdom@gmail.com>
2024-08-12 14:35:16 -0700

    Add logo and link comment. Setup logo link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,14 @@
 const Navbar = () => {
     return (
         <div className = 'h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
-            <div className='1'></div>
+           {/* Logo */}
+            <div className='1'>
+                <Link href="/">
+                    
+                </Link>
+            </div>
+
+            {/* Responsive Menu */}
             <div className='2'></div>
         
         </div>
