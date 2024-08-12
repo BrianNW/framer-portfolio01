da65a5150500ae553d6ee8917f28d5785eb5bc93
Brian <briantechdom@gmail.com>
2024-08-11 19:26:37 -0700

    Add two sample items

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,8 @@
 const Navbar = () => {
     return (
         <div className = 'h-full flex items-center justify-between'>
-            
+            <div className='1'></div>
+            <div className='2'></div>
         
         </div>
     )
