18a296e9fbcf3c62087679367c105f10d821f90e
Brian <briantechdom@gmail.com>
2024-08-14 15:30:18 -0700

    Add menu button container and 3 div containers

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,14 +5,19 @@ const Navbar = () => {
             <div className='1'>
                 <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
                     <span className="text-white">Briannw</span>
-                    <span className ="w-12 h-8 rounded bg-white text-black">.com</span>
+                    <span className ="w-12 h-8 rounded bg-white text-black flex items-center justify-center">.com</span>
                 </Link>
             </div>
 
             {/* Responsive Menu */}
             <div className='2'></div>
-        
-        </div>
+                {/* Menu Button */}
+                <button>
+                    <div clasName="w-10 h-1 bg-black rounded"></div>    
+                    <div clasName="w-10 h-1 bg-black rounded"></div>    
+                    <div clasName="w-10 h-1 bg-black rounded"></div>    
+                </button>                
+            </div>
     )
 }
 
