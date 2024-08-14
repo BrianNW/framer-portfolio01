0bfaf356e1ec1db242ee756e4793a6c350adcdf3
Brian <briantechdom@gmail.com>
2024-08-14 15:05:46 -0700

    Add center and justify

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
         <div className = 'h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
            {/* Logo */}
             <div className='1'>
-                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex">
+                <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
                     <span className="text-white">Briannw</span>
                     <span className ="w-12 h-8 rounded bg-white text-black">.com</span>
                 </Link>
