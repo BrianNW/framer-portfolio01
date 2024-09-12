0edf9a6405c862a806b1a7fd62286cc2cb513c9d
Brian <briantechdom@gmail.com>
2024-09-12 15:09:23 -0700

    Replace Link tags with NavLink component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,9 +16,10 @@ const Navbar = () => {
 
     return (
         <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+
             <div className="hidden md:flex gap-4 w-1/3">
                 {links.map(link => (
-                    <Link href={link.url} key={link.title}> {link.title} </Link>
+                    <NavLink link={link} key={link.title}/>
                 ))}
             </div>
            {/* Logo */}
