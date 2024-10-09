93d035bb019e8a484555ef9353f5bac208b06fc7
Brian <briantechdom@gmail.com>
2024-10-09 15:13:33 -0700

    Add relative for the image's parent container

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Homepage = () => {
   return <div className='flex flex-col'>
     {/* IMAGE CONTAINER */}
-      <div className='h-1/2'>
+      <div className='h-1/2 relative'>
       <Image src="/hero.png" alt="" fill/>
       </div>
     {/* TEXT CONTAINER */}
