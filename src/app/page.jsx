f6aebfa0ae7456b83f593259e08ebb444cff518b
Brian <briantechdom@gmail.com>
2024-10-09 15:16:59 -0700

    Update heigh calc. Add aspect ratio class name object-contain

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ const Homepage = () => {
   return <div className='flex flex-col'>
     {/* IMAGE CONTAINER */}
       <div className='h-1/2 relative'>
-      <Image src="/hero.png" alt="" fill/>
+      <Image src="/hero.png" alt="" fill className="object-contain"/>
       </div>
     {/* TEXT CONTAINER */}
     <div className='h-1/2'></div>
