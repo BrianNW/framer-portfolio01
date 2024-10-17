e963cb75e0d6d3e59c8e87ce30608bb3e98279d1
Brian <briantechdom@gmail.com>
2024-10-17 15:32:43 -0700

    minor syntax adjustments

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
       <Image src="/hero.png" alt="" fill className="object-contain"/>
       </div>
     {/* TEXT CONTAINER */}
-    <div className='h-1/2 flex flex-column gap-8 items-center justify-center'>
+    <div className='h-1/2 flex flex-col gap-8 items-center justify-center'>
     {/* TITLE */}
     <h1 className='text-4xl font-bold'>Transforming ideas into pixel perfect reality</h1>
     <p>Lorem Ipsum placeholder until I figure out better text to add</p>
