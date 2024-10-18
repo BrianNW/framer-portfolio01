33dd05f6b421c4e64bc966fa1c31b45a16d8ee74
Brian <briantechdom@gmail.com>
2024-10-18 14:52:30 -0700

    Create two columns for both containers in large screens

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 
 const Homepage = () => {
-  return <div className='h-full flex flex-col px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+  return <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
     {/* IMAGE CONTAINER */}
-      <div className='h-1/2 relative'>
+      <div className='h-1/2 lg:h-full lg:w-1/2 relative'>
       <Image src="/hero.png" alt="" fill className="object-contain"/>
       </div>
     {/* TEXT CONTAINER */}
-    <div className='h-1/2 flex flex-col gap-8 items-center justify-center'>
+    <div className='h-1/2 lg:h-full lg:w-1/2  flex flex-col gap-8 items-center justify-center'>
     {/* TITLE */}
     <h1 className='text-4xl md:text-6xl font-bold'>Transforming ideas into pixel perfect reality</h1>
     <p className="md:text-xl"> Lorem Ipsum placeholder until I figure out better text to add</p>
