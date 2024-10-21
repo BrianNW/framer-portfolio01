f98c7f4b52c86d835e4e2f5833dbd93ce841d824
Brian <briantechdom@gmail.com>
2024-10-21 14:25:00 -0700

    Update width of the paragraph and buttons to full width on on all screen sizes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,12 +7,12 @@ const Homepage = () => {
       <Image src="/hero.png" alt="" fill className="object-contain"/>
       </div>
     {/* TEXT CONTAINER */}
-    <div className='h-1/2 lg:h-full lg:w-1/2  flex flex-col gap-8 items-center justify-center'>
+    <div className='h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center'>
     {/* TITLE */}
     <h1 className='text-4xl md:text-6xl font-bold'>Transforming ideas into pixel perfect reality</h1>
-    <p className="md:text-xl"> Lorem Ipsum placeholder until I figure out better text to add</p>
+    <p className="w-full  md:text-xl"> Lorem Ipsum placeholder until I figure out better text to add</p>
     {/* BUTTONS */}
-    <div className="flex gap-4">
+    <div className="w-full flex gap-4">
       <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">Portfolio</button>
       <button className="p-4 rounded-lg ring-1 ring-black">Contact</button>
     </div>
