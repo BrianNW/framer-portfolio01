3db8905355035771b062a4cf07d1a1fac596244d
Brian <briantechdom@gmail.com>
2024-10-22 15:16:21 -0700

    Add animation and use client prefix

--- src/app/test-animation/page.js
+++ src/app/test-animation/page.js
@@ -1,10 +1,14 @@
+"use client"
+
 import React from 'react'
 import {motion} from 'framer-motion'
 
 const TestPage = () => {
   return (
     <div className="h-full flex items-center justify-center">
-     <motion.div className="h-96 w-96 bg-red-600 rounded">
+     <motion.div className="h-96 w-96 bg-red-600 rounded" 
+     initial={{x:-100}}
+     animate={{x:100}}>
 
      </motion.div>
     </div>
