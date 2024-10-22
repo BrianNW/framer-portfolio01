0e01bc4e4717be52bf4084e89015b6703840672c
Brian <briantechdom@gmail.com>
2024-10-22 15:06:37 -0700

    add test page.js, TestPage component and motion div tags

---
+++ src/app/test-animation/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+const TestPage = () => {
+  return (
+    <div className="h-full flex items-center justify-center">
+     <motion.div className="h-96 w-96 bg-red-600 rounded">
+
+     </motion.div>
+    </div>
+  )
+}
+
+export default TestPage
