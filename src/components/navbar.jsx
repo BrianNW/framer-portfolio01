649303b83caa86a15a35da71be5ff033444322c1
Brian <briantechdom@gmail.com>
2024-10-24 15:58:18 -0700

    Add motion div element for menu nav list

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -85,14 +85,14 @@ const Navbar = () => {
                 
                 {/* MENU LIST  */}
                 {open && (                
-                    <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
+                    <motion.div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
                         {links.map( link => (
                             <Link href={link.url} key={link.title}>
                                 {link.title}
                             </Link>
                         ))}
                     
-                    </div>
+                    </motion.div>
                 )}
               </div>
             </div>
