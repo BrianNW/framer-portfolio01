cd341370d37466ea5e79f1cfae9b799564fd6c96
Brian <briantechdom@gmail.com>
2024-10-23 17:06:06 -0700

    Add z-index to image container to fix it from popping above mobile menu

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Homepage = () => {
   return <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
     {/* IMAGE CONTAINER */}
-      <div className='h-1/2 lg:h-full lg:w-1/2 relative'>
+      <div className='z-0 h-1/2 lg:h-full lg:w-1/2 relative'>
       <Image src="/hero.png" alt="" fill className="object-contain"/>
       </div>
     {/* TEXT CONTAINER */}
