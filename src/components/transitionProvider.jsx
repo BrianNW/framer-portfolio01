281aed126a60ae10ccf6eeb0a409238dd3f672a8
Brian <briantechdom@gmail.com>
2024-11-20 16:17:59 -0800

    add initial styles

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -9,7 +9,7 @@ const TransitionProvider = ({children}) => {
   return (
     <AnimatePresence>
         <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
-          <motion.div />
+          <motion.div className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"/>
           <div className ="h-24">
             <Navbar />
           </div>
