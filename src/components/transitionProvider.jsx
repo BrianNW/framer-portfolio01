66bbe6437b652152ded234bd9d724196ac16f70d
Brian <briantechdom@gmail.com>
2024-11-23 13:37:56 -0800

    Add animate params

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -9,7 +9,8 @@ const TransitionProvider = ({children}) => {
   return (
     <AnimatePresence>
         <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
-          <motion.div className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"/>
+          <motion.div className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+          animate={{height:"0vh"}} exit={{height:"100vh"}} transition={{duration:0.5, ease:"easeOut"}}/>
           <div className ="h-24">
             <Navbar />
           </div>
