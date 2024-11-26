5b3bb8b836fd07535513a663dc398988088f2a5b
Brian <briantechdom@gmail.com>
2024-11-26 14:53:28 -0800

    Setup pathName, assign parent div unique key for the pathname

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -4,11 +4,15 @@ import { AnimatePresence } from 'framer-motion'
 import React from 'react'
 import Navbar from './navbar'
 import {motion} from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 const TransitionProvider = ({children}) => {
+
+  const pathName = usePathname()
+
   return (
     <AnimatePresence>
-        <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
+        <div key={pathName} className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
           <motion.div className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
           animate={{height:"0vh"}} exit={{height:"100vh"}} transition={{duration:0.5, ease:"easeOut"}}/>
           <div className ="h-24">
