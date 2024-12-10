80c584fb6319ef5e7b96f72a319337ff1d61523c
Brian <briantechdom@gmail.com>
2024-12-10 15:24:02 -0800

    Add new motion div as a second page animation

--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -11,10 +11,13 @@ const TransitionProvider = ({children}) => {
   const pathName = usePathname()
 
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
         <div key={pathName} className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-100">
           <motion.div className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
-          animate={{height:"0vh"}} exit={{height:"100vh"}} transition={{duration:0.5, ease:"easeOut"}}/>
+          animate={{height:"0vh"}} exit={{height:"140vh"}} transition={{duration:0.5, ease:"easeOut"}}/>
+          
+          <motion.div className="h-screen w-screen fixed bg-red-500 rounded-t-[100px] bottom-0 z-40"
+          initial={{height:"140vh"}} animate={{height:"0vh, transition: {delay: 2}"}}/>
           <div className ="h-24">
             <Navbar />
           </div>
