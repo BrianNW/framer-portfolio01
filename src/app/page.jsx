3f790eceef8ea381aca0a2b88e32532884dd5517
Brian <briantechdom@gmail.com>
2024-12-11 13:40:33 -0800

    Add framer motion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
+"use client"
+
 import Image from 'next/image'
+import {motion} from 'framer-motion'
 
 const Homepage = () => {
-  return <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
+  return <div.motion> 
+    <div className='h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
     {/* IMAGE CONTAINER */}
       <div className='h-1/2 lg:h-full lg:w-1/2 relative'>
       <Image src="/hero.png" alt="" fill className="object-contain"/>
@@ -18,6 +22,7 @@ const Homepage = () => {
     </div>
     </div>
   </div>
+  </div.motion>
 };
 
 export default Homepage;
