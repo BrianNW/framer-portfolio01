e063034d754dcb5b1633d6133d97f177d4e00cbc
Brian <briantechdom@gmail.com>
2024-12-14 15:18:44 -0800

    Add container titles

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,11 +15,11 @@ const AboutPage = () => {
             {/* TEXT CONTAINER */}
             <div className="p-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col gap-24">
                 {/* BIOGRAPHY CONTAINER */}
-                <div className=""></div>
+                <div className="">Bio</div>
                 {/* SKILLS CONTAINER */}
-                <div className=""></div>
+                <div className="">Skills</div>
                 {/* EXPERIENCE CONTAINER */}
-                <div className=""></div>
+                <div className="">Experience</div>
             </div>
 
             {/* SVG CONTAINER */}
