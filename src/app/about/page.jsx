6763787dd4f0c1c5d41288bdc3b8a78031e19638
Brian <briantechdom@gmail.com>
2024-12-14 15:17:15 -0800

    Add classes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -13,7 +13,7 @@ const AboutPage = () => {
         {/* CONTAINER */}
         <div className="">
             {/* TEXT CONTAINER */}
-            <div className="">
+            <div className="p-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col gap-24">
                 {/* BIOGRAPHY CONTAINER */}
                 <div className=""></div>
                 {/* SKILLS CONTAINER */}
