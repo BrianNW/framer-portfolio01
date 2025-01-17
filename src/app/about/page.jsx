0a0a2c04d5c6ff0eb4c2c2eac382ae7210947a42
Brian <briantechdom@gmail.com>
2025-01-17 14:04:43 -0800

    Add classes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,16 +16,17 @@ const AboutPage = () => {
             <div className="p-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
                 {/* BIOGRAPHY CONTAINER */}
                 <div className="flex flex-col gap-12 justify-center">
-                    <h1>BIOGRAPHY</h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
+                    <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                         Inventore laboriosam, rem eligendi officia nihil, deserunt nisi ex neque corrupti id eos maiores, aliquam maxime fugit aspernatur amet ut culpa dignissimos!
                     </p>
                     {/* BIOGRAPHY SIGNATURE */}
-                    <span>
+                    <span className="italic">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius mollitia harum et.
                     </span>
-                    <div className="">
+                    
                 {/* SVG BUTTON */}
+                <div className="self-end">
                         <svg>tMy Signature</svg>
                     </div>
                 </div>
