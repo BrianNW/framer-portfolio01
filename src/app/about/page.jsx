e3cda7092254b9ddb503d64bc240165149251d64
Brian <briantechdom@gmail.com>
2025-01-21 13:20:50 -0800

    Add skills container styles

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -16,6 +16,7 @@ const AboutPage = () => {
             <div className="p-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
                 {/* BIOGRAPHY CONTAINER */}
                 <div className="flex flex-col gap-12 justify-center">
+                    {/* BIOGRAPHY TITLE */}
                     <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
                     <p className="text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                         Inventore laboriosam, rem eligendi officia nihil, deserunt nisi ex neque corrupti id eos maiores, aliquam maxime fugit aspernatur amet ut culpa dignissimos!
@@ -31,7 +32,7 @@ const AboutPage = () => {
                     </div>
                 </div>
                 {/* SKILLS CONTAINER */}
-                <div className="">Skills</div>
+                <div className="flex flex-col gap-12 justify-center">Skills</div>
                 {/* EXPERIENCE CONTAINER */}
                 <div className="">Experience</div>
             </div>
