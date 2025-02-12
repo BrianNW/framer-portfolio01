a6d9985a1c38a52047106479b139eba23adf7fda
Brian <briantechdom@gmail.com>
2025-02-11 17:23:03 -0800

    Update experience section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -114,70 +114,106 @@ const AboutPage = () => {
                 </div>
 
                 {/* EXPERIENCE CONTAINER */}
-                <div className="flex flex-col gap-12 justify-center pb-48">
-                    {/* EXPERIENCE TITLE */}
-                    <h1 className="font-bold text-2xl">EXPERIENCE</h1>
-                    {/* EXPERIENCE LIST */}
-                    <div className="">
-                    {/* EXPERIENCE LIST ITEM */} 
-                    <div className="">
-                    {/* LEFT */}                       
-                    <div className="">
-                        {/* JOB TITLE */}
-                        <div className="">Freelancer</div>
-                        {/* JOB DESC */}
-                        <div className="">
-                            Current employment
-                        </div>
-                        {/* JOB DATE */}
-                        <div className="">2024-Present</div>
-                        {/* JOB COMPANY */}
-                        <div className="">Self Employed</div>
-                    </div>
-                        {/* CENTER */}
-                        <div className="">
-                        {/* LINE */}
-                        <div className=""> 
-                            <div className="">
-                                {/* LINE CIRCLE */}
-                            </div>
-                        </div>                        
-                    </div>
-                        {/* RIGHT */}
-                        <div className=""></div>
-                        </div>
-                        {/* EXPERIENCE LIST ITEM */} 
-                        <div className="">
-                        {/* LEFT */}                       
-                        <div className="">
-                            
-                        </div>
-                            {/* CENTER */}
-                            <div className="">
-                            {/* LINE */}
-                            <div className=""> 
-                                <div className="">
-                                    {/* LINE CIRCLE */}
-                                </div>
-                            </div>                        
-                        </div>
-                        {/* RIGHT */}
-                        <div className="">
-                            {/* JOB TITLE */}
-                            <div className="">Freelancer</div>
-                            {/* JOB DESC */}
-                            <div className="">
-                                Current employment
-                            </div>
-                            {/* JOB DATE */}
-                            <div className="">2024-Present</div>
-                            {/* JOB COMPANY */}
-                            <div className="">Self Employed</div>
-                        </div>
-                     </div>
+          <div
+            className="flex flex-col gap-12 justify-center pb-48"
+            
+          >
+            {/* EXPERIENCE TITLE */}
+            
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    White Label Solutions
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I provide White Label solutions in both Web Development and SEO for organizations looking to offload work but keep branding {" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2022 - Present
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    {/* Company Name here */}
                   </div>
                 </div>
-            </div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 "></div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 "></div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Search Engine Optimzation
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I develop strategies and implement standard practices for effective on and off page SEO{" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2020 - Present{" "}
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    {/* Company Name here */}
+                  </div>
+                </div>
+              </div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    Freelance Web Development{" "}
+                  </div>
+                  {/* JOB DESC */}
+                  <div className="p-3 text-sm italic">
+                    I provided cost effective Web Solutions like Websites for small to medium businesses{" "}
+                  </div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    2013 - Present{" "}
+                  </div>
+                </div>
+                {/* CENTER */}
+                <div className="w-1/6 flex justify-center">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 "></div>
+              </div>            
+          </div>
+        </div>
             {/* SVG CONTAINER */}
             <div className="hidden"></div>
 
